180228fe598a8f3d8b46889012b5c169a7c1c57f
jazino62 <jazino62@gmail.com>
2018-09-26 01:41:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,7 +12,7 @@ def display_board(board)
 end
 
   board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
-  def display_board(board)
+  #def display_board(board)
 
   #board[4] = "X"
     puts "   |   |   "
