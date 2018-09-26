e98c87fd058db8803595a8abee3d6d4201dc903d
jazino62 <jazino62@gmail.com>
2018-09-26 01:12:31 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,17 +137,7 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
-      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 
-      output = capture_puts{ display_board(board) }
-      rows = output.split("\n")
-
-      expect(rows[0]).to eq(" X | X | X ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" X | X | X ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" X | X | X ")
-    end
       # Should you want to write your own test for this situation,
       # read the following code and comments.
 
@@ -156,7 +146,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] # This is not correct
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
@@ -176,11 +166,11 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-      # expect(rows[0]).to eq("   |   |   ")
-      # expect(rows[1]).to eq("-----------")
-      # expect(rows[2]).to eq("   |   |   ")
-      # expect(rows[3]).to eq("-----------")
-      # expect(rows[4]).to eq("   |   |   ")
+       expect(rows[0]).to eq(" X | X | X ")
+       expect(rows[1]).to eq("-----------")
+       expect(rows[2]).to eq(" X | X | X ")
+       expect(rows[3]).to eq("-----------")
+       expect(rows[4]).to eq(" X | X | X ")
 
       # *** Comment the line below by adding a # at the line start ***
       expect(true).to be(true)
