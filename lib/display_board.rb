fa6c9d892edbf379f5bb6ec2bdce6f3367f216a6
jazino62 <jazino62@gmail.com>
2018-09-26 01:23:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  #puts " Welcome to Tic Tac Toe"
+  
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
